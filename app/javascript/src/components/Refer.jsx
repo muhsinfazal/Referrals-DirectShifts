21e94647b0024fdc2bf143fdbfcf9368a2b1a5c3
Muhsin Ahamed Fazal <muhsin.fazal@bigbinary.com>
2023-04-25 10:46:31 +0530

    Added form validation for referral email

--- app/javascript/src/components/Refer.jsx
+++ app/javascript/src/components/Refer.jsx
@@ -11,67 +11,74 @@ import referralsApi from "apis/referrals";
 import UserContext from "contexts/UserContext";
 
 import { toast } from "material-react-toastify";
+import { Form, Formik } from "formik";
+import { emailValidationShema } from "./validations";
 
 const Refer = () => {
   const currentUser = useContext(UserContext);
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-
+  const handleSubmit = async (values) => {
     try {
       await referralsApi.create({
         from_id: currentUser?.id,
-        to: data.get("email")
+        to: values.email
       });
       toast.success("Referred successfully!", { theme: "colored" });
     } catch (err) {
       logger.log(err);
       toast.error("Referral failed!", { theme: "colored" });
     }
+    return;
   };
 
   return (
-    <>
-      <NavBar />
-      <Box
-        sx={{
-          marginTop: 8,
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center"
-        }}
-      >
-        <Typography component="h1" variant="h5" sx={{ fontWeight: "bold" }}>
-          Refer a friend
-        </Typography>
-        <Box
-          component="form"
-          onSubmit={handleSubmit}
-          noValidate
-          sx={{ mt: 1, width: "50%" }}
-        >
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label="Email Address"
-            name="email"
-            autoComplete="email"
-            autoFocus
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            sx={{ mt: 3, mb: 2 }}
+    <Formik
+      initialValues={{ email: "" }}
+      validationSchema={emailValidationShema}
+      onSubmit={handleSubmit}
+    >
+      {(formik) => (
+        <Form>
+          <NavBar />
+          <Box
+            sx={{
+              marginTop: 8,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center"
+            }}
           >
-            Refer
-          </Button>
-        </Box>
-      </Box>
-    </>
+            <Typography component="h1" variant="h5" sx={{ fontWeight: "bold" }}>
+              Refer a friend
+            </Typography>
+            <Box sx={{ mt: 1, width: "50%" }}>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+                value={formik.values.email}
+                onChange={formik.handleChange}
+                error={formik.touched.email && Boolean(formik.errors.email)}
+                helperText={formik.touched.email && formik.errors.email}
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Refer
+              </Button>
+            </Box>
+          </Box>
+        </Form>
+      )}
+    </Formik>
   );
 };
 
